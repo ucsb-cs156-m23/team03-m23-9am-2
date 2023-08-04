9bb51683e8f096e25c9f24d992500024299781f7
Mohamed-Elfouly <elfouly@ucsb.edu>
2023-08-04 00:09:53 -0700

    Fixed Page Tests
    I forgot to change placeHolder to HelpRequest

--- frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
+++ frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
+import HelpRequestCreatePage from "main/pages/HelpRequest/HelpRequestCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
@@ -8,7 +8,7 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-describe("PlaceholderCreatePage tests", () => {
+describe("HelpRequestCreatePage tests", () => {
 
     const axiosMock = new AxiosMockAdapter(axios);
 
@@ -29,7 +29,7 @@ describe("PlaceholderCreatePage tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <PlaceholderCreatePage />
+                    <HelpRequestCreatePage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
