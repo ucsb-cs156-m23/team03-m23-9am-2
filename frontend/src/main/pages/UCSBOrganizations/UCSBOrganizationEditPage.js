886350549bc58aa4452d1ea5d5f03c7f7bb0c64a
Andrew Thai <andrewthai@Andrews-MacBook-Air.local>
2023-08-13 20:51:16 -0700

    just updating

--- frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationEditPage.js
+++ frontend/src/main/pages/UCSBOrganizations/UCSBOrganizationEditPage.js
@@ -6,12 +6,12 @@ import { useBackend, useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 
 export default function UCSBOrganizationEditPage({storybook=false}) {
-    let { id } = useParams();
+    let { orgCode } = useParams();
 
     const { data: organization, _error, _status } =
         useBackend(
             // Stryker disable next-line all : don't test internal caching of React Query
-            [`/api/ucsborganization?id=${orgCode}`],
+            [`/api/ucsborganization?orgCode=${orgCode}`],
             {  // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
                 method: "GET",
                 url: `/api/ucsborganization`,
@@ -35,14 +35,14 @@ export default function UCSBOrganizationEditPage({storybook=false}) {
     });
 
     const onSuccess = (organization) => {
-        toast(`UCSBOrganization Updated - id: ${organization.orgCode}`);
+        toast(`UCSBOrganization Updated - orgCode: ${organization.orgCode}`);
     }
 
     const mutation = useBackendMutation(
         objectToAxiosPutParams,
         { onSuccess },
         // Stryker disable next-line all : hard to set up test for caching
-        [`/api/ucsborganization?id=${orgCode}`]
+        [`/api/ucsborganization?orgCode=${orgCode}`]
     );
 
     const { isSuccess } = mutation
--- frontend/src/tests/pages/UCSBOrganizations/UCSBOrganizationEditPage.test.js
+++ frontend/src/tests/pages/UCSBOrganizations/UCSBOrganizationEditPage.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganizations/UCSBOrganizationEditPage";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
@@ -26,13 +26,13 @@ jest.mock('react-router-dom', () => {
         __esModule: true,
         ...originalModule,
         useParams: () => ({
-            id: 17
+            orgCode: "123"
         }),
         Navigate: (x) => { mockNavigate(x); return null; }
     };
 });
 
-describe("RestaurantEditPage tests", () => {
+describe("UCSBOrganizationEditPage tests", () => {
 
     describe("when the backend doesn't return data", () => {
 
@@ -43,7 +43,7 @@ describe("RestaurantEditPage tests", () => {
             axiosMock.resetHistory();
             axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
             axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-            axiosMock.onGet("/api/restaurants", { params: { id: 17 } }).timeout();
+            axiosMock.onGet("/api/ucsborganization", { params: { orgCode: "123" } }).timeout();
         });
 
         const queryClient = new QueryClient();
@@ -54,12 +54,12 @@ describe("RestaurantEditPage tests", () => {
             render(
                 <QueryClientProvider client={queryClient}>
                     <MemoryRouter>
-                        <RestaurantEditPage />
+                        <UCSBOrganizationEditPage />
                     </MemoryRouter>
                 </QueryClientProvider>
             );
-            await screen.findByText("Edit Restaurant");
-            expect(screen.queryByTestId("Restaurant-name")).not.toBeInTheDocument();
+            await screen.findByText("Edit UCSBOrganization");
+            expect(screen.queryByTestId("UCSBOrganization-orgCode")).not.toBeInTheDocument();
             restoreConsole();
         });
     });
@@ -73,15 +73,17 @@ describe("RestaurantEditPage tests", () => {
             axiosMock.resetHistory();
             axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
             axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-            axiosMock.onGet("/api/restaurants", { params: { id: 17 } }).reply(200, {
-                id: 17,
-                name: "Freebirds",
-                description: "Burritos"
+            axiosMock.onGet("/api/ucsborganization", { params: { orgCode: "123" } }).reply(200, {
+                orgCode: "123",
+                orgTranslationShort: "abc",
+                orgTranslation: "ay bruh cool",
+                inactive: false
             });
-            axiosMock.onPut('/api/restaurants').reply(200, {
-                id: "17",
-                name: "Freebirds World Burrito",
-                description: "Really big Burritos"
+            axiosMock.onPut('/api/ucsborganization').reply(200, {
+                orgCode: "123",
+                orgTranslationShort: "aaa",
+                orgTranslation: "andrew ate app",
+                inactive: true
             });
         });
 
@@ -92,41 +94,41 @@ describe("RestaurantEditPage tests", () => {
             render(
                 <QueryClientProvider client={queryClient}>
                     <MemoryRouter>
-                        <RestaurantEditPage />
+                        <UCSBOrganizationEditPage />
                     </MemoryRouter>
                 </QueryClientProvider>
             );
 
-            await screen.findByTestId("RestaurantForm-id");
+            await screen.findByTestId("UCSBOrganizationForm-orgCode");
 
-            const idField = screen.getByTestId("RestaurantForm-id");
-            const nameField = screen.getByTestId("RestaurantForm-name");
-            const descriptionField = screen.getByTestId("RestaurantForm-description");
-            const submitButton = screen.getByTestId("RestaurantForm-submit");
+            const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+            const orgTranslationShortField = screen.getByTestId("UCSBOrganizationForm-orgTranslationShort");
+            const orgTranslationField = screen.getByTestId("UCSBOrganizationForm-orgTranslation");
+            const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
+            const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
 
-            expect(idField).toBeInTheDocument();
-            expect(idField).toHaveValue("17");
-            expect( nameField).toBeInTheDocument();
-            expect(nameField).toHaveValue("Freebirds");
-            expect(descriptionField).toBeInTheDocument();
-            expect(descriptionField).toHaveValue("Burritos");
+            expect(orgCodeField).toBeInTheDocument();
+            expect(orgCodeField).toHaveValue("123");
+            expect(orgTranslationShortField).toBeInTheDocument();
+            expect(orgTranslationShortField).toHaveValue("abc");
+            expect(orgTranslationField).toBeInTheDocument();
+            expect(orgTranslationField).toHaveValue("ay bruh cool");
 
-            expect(submitButton).toHaveTextContent("Update");
-
-            fireEvent.change(nameField, { target: { value: 'Freebirds World Burrito' } });
-            fireEvent.change(descriptionField, { target: { value: 'Totally Giant Burritos' } });
-            fireEvent.click(submitButton);
+            fireEvent.change(orgTranslationShortField, { target: { value: 'aaa' } });
+            fireEvent.change(orgTranslationField, { target: { value: 'andrew ate app' } });
+            fireEvent.change(inactiveField, { target: { value: true } });
 
             await waitFor(() => expect(mockToast).toBeCalled());
-            expect(mockToast).toBeCalledWith("Restaurant Updated - id: 17 name: Freebirds World Burrito");
+            expect(mockToast).toBeCalledWith("UCSBOrganization Updated - orgCode: 123");
             
-            expect(mockNavigate).toBeCalledWith({ "to": "/restaurants" });
+            expect(mockNavigate).toBeCalledWith({ "to": "/ucsborganization" });
 
             expect(axiosMock.history.put.length).toBe(1); // times called
-            expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+            expect(axiosMock.history.put[0].params).toEqual({ orgCode: "123" });
             expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
-                name: 'Freebirds World Burrito',
-                description: 'Totally Giant Burritos'
+                orgTranslationShort: 'aaa',
+                orgTranslation: 'andrew ate app',
+                inactive: false
             })); // posted object
 
 
@@ -137,31 +139,33 @@ describe("RestaurantEditPage tests", () => {
             render(
                 <QueryClientProvider client={queryClient}>
                     <MemoryRouter>
-                        <RestaurantEditPage />
+                        <UCSBOrganizationEditPage />
                     </MemoryRouter>
                 </QueryClientProvider>
             );
 
-            await screen.findByTestId("RestaurantForm-id");
+            await screen.findByTestId("UCSBOrganizationForm-orgCode");
 
-            const idField = screen.getByTestId("RestaurantForm-id");
-            const nameField = screen.getByTestId("RestaurantForm-name");
-            const descriptionField = screen.getByTestId("RestaurantForm-description");
-            const submitButton = screen.getByTestId("RestaurantForm-submit");
+            const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+            const orgTranslationShortField = screen.getByTestId("UCSBOrganizationForm-orgTranslationShort");
+            const orgTranslationField = screen.getByTestId("UCSBOrganizationForm-orgTranslation");
+            const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
+            const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
 
-            expect(idField).toHaveValue("17");
-            expect(nameField).toHaveValue("Freebirds");
-            expect(descriptionField).toHaveValue("Burritos");
+            expect(orgCodeField).toHaveValue("123");
+            expect(orgTranslationShortField).toHaveValue("abc");
+            expect(orgTranslationField).toHaveValue("ay bruh cool");
             expect(submitButton).toBeInTheDocument();
 
-            fireEvent.change(nameField, { target: { value: 'Freebirds World Burrito' } })
-            fireEvent.change(descriptionField, { target: { value: 'Big Burritos' } })
+            fireEvent.change(orgTranslationShortField, { target: { value: 'aaa' } });
+            fireEvent.change(orgTranslationField, { target: { value: 'andrew ate app' } });
+            fireEvent.change(inactiveField, { target: { value: true } });
 
             fireEvent.click(submitButton);
 
             await waitFor(() => expect(mockToast).toBeCalled());
-            expect(mockToast).toBeCalledWith("Restaurant Updated - id: 17 name: Freebirds World Burrito");
-            expect(mockNavigate).toBeCalledWith({ "to": "/restaurants" });
+            expect(mockToast).toBeCalledWith("UCSBOrganization Updated - orgCode: 123");
+            expect(mockNavigate).toBeCalledWith({ "to": "/ucsborganization" });
         });
 
        
